525b95c0c0449fa6ab152905b3f0f1e9022c2e5f
Lenin Paulino <leninpaulinon@gmail.com>
2021-05-04 04:06:53 -0400

    feat: switching between slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-function Slides({slides}) {
-
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+function Slide({ title, text }) {
+  return (
+    <div id="slide" className="card text-center">
+      <h1 data-testid="title">{title}</h1>
+      <p data-testid="text">{text}</p>
+    </div>
+  )
 }
 
-export default Slides;
+function Slides({ slides }) {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const { title, text } = slides[currentSlide]
+
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          disabled={currentSlide === 0}
+          onClick={() => setCurrentSlide(0)}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          disabled={currentSlide === 0}
+          onClick={() => setCurrentSlide(currentSlide - 1)}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          disabled={currentSlide === slides.length - 1}
+          onClick={() => setCurrentSlide(currentSlide + 1)}
+        >
+          Next
+        </button>
+        <Slide title={title} text={text} />
+      </div>
+    </div>
+  )
+}
+
+export default Slides
